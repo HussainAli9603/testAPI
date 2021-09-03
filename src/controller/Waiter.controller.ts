5662603dce6b37651bf4eb81e2487ccf8936eba6
HussainAli9603 <72409657+HussainAli9603@users.noreply.github.com>
2021-09-03 05:07:22 -0700

    jwt

--- src/controller/Waiter.controller.ts
+++ src/controller/Waiter.controller.ts
@@ -10,19 +10,21 @@ import { IOrder } from '../types/document/IOrder';
 @Tags('waiter')
 export class WaiterController {
     constructor() { }
-
+    
     @Post('/waiter/login')
     async loginWaiterEmail(@Body() loginEmail: ReqLoginWaiter): Promise<ResWaiter> {
         const login_waiter: any = await new MainAdmin().WaiterEmailFind(<IWaiter>(loginEmail));
         return <ResWaiter>(login_waiter)
     }
-
+    
+    @Security("jwt")
     @Post("/add/order")
     async SaveOrder(@Body() getreq: ReqOrder): Promise<ResOrder> {
         const order: any = await new MainAdmin().addOrder(<IOrder>(getreq));
         return <ResOrder>order;
     }
 
+    @Security("jwt")
     @Post("/get/waiter-order")
     async waiterOrder(@Body() getreq: ReqWaiterOrderById): Promise<ResOrder> {
         const allwaiterorder: any = await new MainAdmin().getWaiterOrder(getreq._id);
